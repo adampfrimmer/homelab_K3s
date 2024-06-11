e5923be85d3fc29536b184cde43b3d112c520507
adampfrimmer <pfrimmeradam@gmail.com>
2024-06-11 22:40:00 +0000

    add instructions for bootstrapping dev

---
+++ dev-instructions/install_additional_apps.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+kubectl create namespace gluetun
+
+kubectl create secret generic wireguard -n gluetun \
+--from-literal=PIA_USER="" \
+--from-literal=PIA_PASS=""
+
+
+argocd app create gluetun \
+  --repo https://github.com/adampfrimmer/homelab_K3s.git \
+  --path gluetun-wireguard-qbittorrent/repo\
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace gluetun \
+  --sync-policy automated \
+  --revision dev
+
+
+GLUETUN_IP=$(kubectl get svc gluetun-wireguard-qbittorrent-service -n gluetun -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+log_line=$(kubectl logs -n gluetun $(kubectl get pods -n gluetun -l app=gluetun-wireguard-qbittorrent -o=jsonpath='{.items[0].metadata.name}') -c qbittorrent | grep -o 'A temporary password is provided for this session: [[:alnum:]]*')
+password=$(echo "$log_line" | grep -oP 'A temporary password is provided for this session: \K\S+')
+
+echo -e "gluetun-wireguard-qbittorrent is deployed at $GLUETUN_IP:8080 with temporary password: $password \nLogin and change this password under program preferences"
+
+
+
+
+kubectl create namespace unifi
+
+kubectl create secret generic mongo-creds -n unifi \
+  --from-literal=username= \
+  --from-literal=password= 
+
+argocd app create mongodb \
+  --repo https://github.com/adampfrimmer/homelab_K3s.git \
+  --path unifi-network-application/mongodb \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace unifi \
+  --sync-policy automated \
+  --revision dev
+
+
+argocd app create unifi \
+  --repo https://github.com/adampfrimmer/homelab_K3s.git \
+  --path unifi-network-application/unifi \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace unifi \
+  --sync-policy automated \
+  --revision dev
+
+
+UNIFI_IP=$(kubectl get svc unifi-tcp -n unifi -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo -e "unifi is deployed at $UNIFI_IP:8443"
---
+++ dev-instructions/install_argocd.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+ENCODED_PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}")
+PASSWORD=$(echo $ENCODED_PASSWORD | base64 --decode)
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
+
+argocd login localhost:8080 --username admin --password $PASSWORD --insecure
+
+sudo kubectl config set-context --current --namespace=argocd
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl create namespace nginx-ingress
+argocd app create nginx-ingress \
+  --repo https://github.com/adampfrimmer/homelab_K3s.git \
+  --path nginx-ingress/repo\
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace nginx-ingress \
+  --sync-policy automated \
+  --revision dev
+
+kubectl create namespace metallb
+argocd app create metallb \
+  --repo https://github.com/adampfrimmer/homelab_K3s.git \
+  --path metallb/repo\
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace metallb \
+  --sync-policy automated \
+  --revision dev
+
+ARGOCD_IP=$(kubectl get svc argocd-server -n argocd -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+echo "ARGOCD is deployed at $ARGOCD_IP with admin password: $PASSWORD"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
